657966310bc8fd09ba23440e9011754f30abfeaf
Hugo Baird <hugonzb@gmail.com>
2020-06-21 00:25:08 +1200

    Updates

---
+++ precache-manifest.c0b9ea99076f71ba476dcc6441861050.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e18fe8968d359910ce8e2484fcce222f",
+    "url": "/http//hugonzb.github.io/Imperial-Footwear/index.html"
+  },
+  {
+    "revision": "508c71794961f1661568",
+    "url": "/http//hugonzb.github.io/Imperial-Footwear/static/css/main.2a736839.chunk.css"
+  },
+  {
+    "revision": "dddcbd83b5ea99c0d695",
+    "url": "/http//hugonzb.github.io/Imperial-Footwear/static/js/2.3afe0f7b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/http//hugonzb.github.io/Imperial-Footwear/static/js/2.3afe0f7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "508c71794961f1661568",
+    "url": "/http//hugonzb.github.io/Imperial-Footwear/static/js/main.1a4db252.chunk.js"
+  },
+  {
+    "revision": "27e339842392177f5491",
+    "url": "/http//hugonzb.github.io/Imperial-Footwear/static/js/runtime-main.d80bd917.js"
+  }
+]);
