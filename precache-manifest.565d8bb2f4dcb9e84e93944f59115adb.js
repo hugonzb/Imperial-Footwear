ef2fba9c7b4dbf6da85fe82119aef086b006e8c9
Hugo Baird <hugonzb@gmail.com>
2020-06-21 01:09:50 +1200

    Updates

---
+++ precache-manifest.565d8bb2f4dcb9e84e93944f59115adb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4543002c7532d516dc33ea4875142c44",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0538d8c62be59104cc1d",
+    "url": "/static/css/main.2a736839.chunk.css"
+  },
+  {
+    "revision": "dddcbd83b5ea99c0d695",
+    "url": "/static/js/2.3afe0f7b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3afe0f7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0538d8c62be59104cc1d",
+    "url": "/static/js/main.348dd87a.chunk.js"
+  },
+  {
+    "revision": "b1c7ddff8c9613cdcb9f",
+    "url": "/static/js/runtime-main.fbbc5fd3.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Imperial-Footwear/precache-manifest.b419c28ee574776299405d520a540f6b.js"
+  "/precache-manifest.565d8bb2f4dcb9e84e93944f59115adb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Imperial-Footwear/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
