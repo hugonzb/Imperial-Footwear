8438b73d63ab4f3f380cba60766b795d07ae4b8b
Hugo Baird <hugonzb@gmail.com>
2020-06-20 20:44:27 +1200

    Completed initial HTML and CSS templates

---
+++ front-end-display/src/data.js
@@ -0,0 +1,59 @@
+export default {
+    shoes:[{
+        name: 'Jordan 1 Bloodline',
+        image: '/images/jordan1.jpg',
+        year: 2019,
+        price: 300,
+        brand: 'Jordan',
+        rating: 10
+    },
+    {
+        name: 'Jordan 11 Concord',
+        image: '/images/jordan1.jpg',
+        year: 2018,
+        price: 200,
+        brand: 'Jordan',
+        rating: 5
+    },
+    {
+        name: 'Nike Air Force 1',
+        image: '/images/jordan1.jpg',
+        year: 2010,
+        price: 100,
+        brand: 'Nike',
+        rating: 1
+    },
+    {
+        name: 'Jordan 11 Concord',
+        image: '/images/jordan1.jpg',
+        year: 2018,
+        price: 200,
+        brand: 'Jordan',
+        rating: 5
+    },
+    {
+        name: 'Jordan 11 Concord',
+        image: '/images/jordan1.jpg',
+        year: 2018,
+        price: 200,
+        brand: 'Jordan',
+        rating: 5
+    },
+    {
+        name: 'Jordan 11 Concord',
+        image: '/images/jordan1.jpg',
+        year: 2018,
+        price: 200,
+        brand: 'Jordan',
+        rating: 5
+    },
+    {
+        name: 'Jordan 11 Concord',
+        image: '/images/jordan1.jpg',
+        year: 2018,
+        price: 200,
+        brand: 'Jordan',
+        rating: 5
+    }
+    ]
+}
