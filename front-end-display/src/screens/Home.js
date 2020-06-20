aa8abc576464d082a9a29946e823636f18eef562
Hugo Baird <hugonzb@gmail.com>
2020-06-20 22:03:37 +1200

    Created clickable shoe image

--- front-end-display/src/screens/Home.js
+++ front-end-display/src/screens/Home.js
@@ -8,14 +8,16 @@ function Home (props) {
     data.shoes.map(shoe =>               
       <li> 
           <div className="footwear">
-              <img className="shoe-image" src={shoe.image} alt="shoe"></img>
-              <div className="shoe-name">
-                  <Link to={'/shoe/' + shoe._id}>{shoe.name}</Link>
-              </div>
-              <div className="shoe-brand">{shoe.brand}</div>
-              <div className="shoe-year">{shoe.year}</div>
-              <div className="shoe-price">{shoe.price}</div>
-              <div className="shoe-rating">{shoe.rating}</div>
+                <Link to={'/shoe/' + shoe._id}>
+                <img className="shoe-image" src={shoe.image} alt="shoe"></img>
+                </Link>
+                <div className="shoe-name">
+                    <Link to={'/shoe/' + shoe._id}>{shoe.name}</Link>
+                </div>
+                <div className="shoe-brand">{shoe.brand}</div>
+                <div className="shoe-year">{shoe.year}</div>
+                <div className="shoe-price">{shoe.price}</div>
+                <div className="shoe-rating">{shoe.rating}</div>
           </div>
       </li>)
     }   
