065b1b8a7ce142932b3828fb5e276e33489852be
Hugo Baird <hugonzb@gmail.com>
2020-06-21 01:16:43 +1200

    Updates

---
+++ precache-manifest.dea51f79aa7dfdee36e18e57b9b28df9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2499839ac252c22d85bd725813c0300d",
+    "url": "/Imperial-Footwear/index.html"
+  },
+  {
+    "revision": "68335d39d55545f64497",
+    "url": "/Imperial-Footwear/static/css/main.2a736839.chunk.css"
+  },
+  {
+    "revision": "dddcbd83b5ea99c0d695",
+    "url": "/Imperial-Footwear/static/js/2.3afe0f7b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Imperial-Footwear/static/js/2.3afe0f7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68335d39d55545f64497",
+    "url": "/Imperial-Footwear/static/js/main.635f621f.chunk.js"
+  },
+  {
+    "revision": "88786043f1978fbe9bf0",
+    "url": "/Imperial-Footwear/static/js/runtime-main.ed91aaa0.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.565d8bb2f4dcb9e84e93944f59115adb.js"
+  "/Imperial-Footwear/precache-manifest.dea51f79aa7dfdee36e18e57b9b28df9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Imperial-Footwear/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
