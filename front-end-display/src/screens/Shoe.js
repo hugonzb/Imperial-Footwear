c5836f1faf15d1a8ba07ff6bd9b8800233b92303
Hugo Baird <hugonzb@gmail.com>
2020-07-01 00:09:20 +1200

    Rearanged rating position and reversed order

--- front-end-display/src/screens/Shoe.js
+++ front-end-display/src/screens/Shoe.js
@@ -20,7 +20,7 @@ function Shoe (props) {
     useEffect(() => {
         if (shoeSaveSuccess){
             alert("Review Submitted Successfully.");
-            setRating(0);
+            setRating(1);
             setComment('');
             dispatch({ type: SHOE_REVIEW_SAVE_RESET });
         }
@@ -110,10 +110,41 @@ function Shoe (props) {
             </div>
             </div>
             <div className="content-margined">
+            <li>
+                    <h2>
+                        Write a review for this shoe
+                    </h2>
+                        {userInfo ? <form onSubmit={submitHandler}>
+                            <ul className="form-container">
+                                <li>
+                                    <label htmlFor="rating">
+                                        Rating
+                                    </label>
+                                    <select name="rating" id="rating" value={rating}
+                                    onChange={(e)=> setRating(e.target.value)}>
+                                        <option value="1">1</option>
+                                        <option value="2">2</option>
+                                        <option value="3">3</option>
+                                        <option value="4">4</option>
+                                        <option value="5">5</option>
+                                    </select>
+                                </li>
+                                <li>
+                                    <label htmlFor="comment">Comment</label>
+                                    <textarea name="comment" value={comment} onChange={(e)=>setComment(e.target.value)}></textarea>
+                                </li>
+                                <li>
+                                    <button type="submit" className="button primary">Submit</button>
+                                </li>
+                                </ul>
+                            </form>:
+                            <div>Please <Link to="/signin">Sign In</Link> to write a review for this shoe.</div>}
+                        </li>
+                    <div>
                     <h2> Reviews </h2>
                     {!shoe.reviews.length && <div> There are currently no reviews, be the first? </div>}
                     <ul className="review" id="reviews">
-                        {shoe.reviews.map((review) => (
+                        {shoe.reviews.slice(0).reverse().map((review) => (
                             <li key={review._id}>
                                 <div>
                                     {review.name}
@@ -129,37 +160,8 @@ function Shoe (props) {
                                 </div>
                             </li>
                         ))}
-                        <li>
-                            <h2>
-                                Write a review for this shoe
-                            </h2>
-                            {userInfo ? <form onSubmit={submitHandler}>
-                                <ul className="form-container">
-                                    <li>
-                                        <label htmlFor="rating">
-                                            Rating
-                                        </label>
-                                        <select name="rating" id="rating" value={rating}
-                                        onChange={(e)=> setRating(e.target.value)}>
-                                            <option value="1">1</option>
-                                            <option value="2">2</option>
-                                            <option value="3">3</option>
-                                            <option value="4">4</option>
-                                            <option value="5">5</option>
-                                        </select>
-                                    </li>
-                                    <li>
-                                        <label htmlFor="comment">Comment</label>
-                                        <textarea name="comment" value={comment} onChange={(e)=>setComment(e.target.value)}></textarea>
-                                    </li>
-                                    <li>
-                                        <button type="submit" className="button primary">Submit</button>
-                                    </li>
-                                </ul>
-                            </form>:
-                            <div>Please <Link to="/signin">Sign In</Link> to write a review for this shoe.</div>}
-                        </li>
                     </ul>
+                    </div>
             </div>
             </>
             )
