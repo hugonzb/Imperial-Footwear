d51c41d9ef73d5c4e72d9db6d2ac28e4a325bfac
Hugo Baird <hugonzb@gmail.com>
2020-07-07 17:10:31 +1200

    Improved colour and search bar reponsiveness

--- front-end-display/src/screens/Home.js
+++ front-end-display/src/screens/Home.js
@@ -34,7 +34,7 @@ function Home (props) {
         <ul className="filter">
             <li>
                 <form onSubmit={submitHandler}>
-                    <input name="searchWord" placeholder="Search" onChange={(e) => {setSearchWord(e.target.value)}} />
+                    <input className = "search" name="searchWord" placeholder="Search" onChange={(e) => {setSearchWord(e.target.value)}} />
                     <button className = "cursor-pointer" type = "submit"><FontAwesomeIcon icon={faSearch}/></button>
                 </form>
             </li>
