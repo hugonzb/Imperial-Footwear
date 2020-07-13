ad7bcc27af6b0d8fc8b9710c12ba2dae3ead8837
Hugo Baird <hugonzb@gmail.com>
2020-07-13 15:27:34 +1200

    Deploy

---
+++ front-end-display/src/screens/Checkout.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Checkout () {
+
+    return <div className="checkout">
+        Successfully placed your order. Thank you for shopping with us!
+    </div>
+}
+export default Checkout;
