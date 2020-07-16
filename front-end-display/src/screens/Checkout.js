d76c13609ca28b88af203488b12ffd81a99c67ef
Hugo Baird <hugonzb@gmail.com>
2020-07-16 14:37:57 +1200

    Added buttons to checkout

--- front-end-display/src/screens/Checkout.js
+++ front-end-display/src/screens/Checkout.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Checkout () {
 
     return <div className="checkout">
-        Successfully placed your order. Thank you for shopping with us!
+        <div className="checkout-message">
+            <div>Successfully placed your order.</div>
+            <div>Thank you for shopping with us!</div>
+            <div className="checkout-buttons">
+                <div className ="signreg-submit-button">
+                    <Link to="/" className ="reset-text-decoration"> Order History </Link>
+                </div>
+                <div>&nbsp;</div>
+                <div className ="signreg-submit-button">
+                    <Link to="/" className ="reset-text-decoration"> Home </Link>
+                </div>
+            </div>
+        </div>
     </div>
 }
 export default Checkout;
